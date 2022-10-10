1f18686c8be06f7923952700ab3ac01d9dbb27d3
seu nome <adnanbezerra@gmail.com>
2022-10-10 16:32:35 -0300

    fix: add description to schema

--- src/schemas/NewPostSchema.ts
+++ src/schemas/NewPostSchema.ts
@@ -2,5 +2,6 @@ import Joi from "joi";
 
 export const NewPostSchema = Joi.object({
     name: Joi.string().trim().required(),
-    creationDate: Joi.date().required()
+    creationDate: Joi.date().required(),
+    description: Joi.string().trim().required()
 })
